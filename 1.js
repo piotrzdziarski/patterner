68184d7f26da82d5cf809b6e52d2fd35da0f2fcc
piotrzdziarski <pzdziarski@protonmail.com>
2026-03-10 20:16:42 +0100

    Apply multicolor to Sierpiński's Triangle and Carpet

--- 1.js
+++ 1.js
@@ -1,10 +1,7 @@
-var cn=document.getElementById("c"),k=document.getElementById("kolor"),p=document.getElementById("poziom"),w=cn.width,h=cn.height,c=cn.getContext("2d"),a=(2*h*Math.sqrt(3))/3,wr=(w-a)/2;k.onfocus=()=>k.classList.remove("e");p.onfocus=()=>p.classList.remove("e");document.getElementById("l").onclick=()=>{k.classList.remove("e");p.classList.remove("e");k.value=""+rh()+rh()+rh()+rh()+rh()+rh();var o=p.value,n;while((n=Math.floor(Math.random()*10))==o);p.value=n};document.getElementById("u").onclick=()=>{var kv=k.value,pv=p.value,b=1;if(kv.length!=6){k.classList.add("e");return};for(var i=0;i<6;i++)if(!ih(kv[i])){b=0;break}if(!b)k.classList.add("e");if(pv!="0"&&pv!="1"&&pv!="2"&&pv!="3"&&pv!="4"&&pv!="5"&&pv!="6"&&pv!="7"&&pv!="8"&&pv!="9"){p.classList.add("e");b=0}if(b)Rysuj_Trojkat_Sierpinskiego(kv,pv)};function rh(){var n=Math.floor(Math.random()*16);if(n==10)return"a";if(n==11)return"b";if(n==12)return"c";if(n==13)return"d";if(n==14)return"e";if(n==15)return"f";return n}function ih(h){return h=="0"||h=="1"||h=="2"||h=="3"||h=="4"||h=="5"||h=="6"||h=="7"||h=="8"||h=="9"||h=="a"||h=="A"||h=="b"||h=="B"||h=="c"||h=="C"||h=="d"||h=="D"||h=="e"||h=="E"||h=="f"||h=="F"}Rysuj_Trojkat_Sierpinskiego("f15f04",2);
+var M=9,i,cn=document.getElementById("c"),clb=document.getElementById("clb"),clbi=0,cls=[],cli=document.getElementById("cli"),k=document.getElementById("kolor"),p=document.getElementById("poziom"),w=cn.width,h=cn.height,c=cn.getContext("2d"),a=(2*h*Math.sqrt(3))/3,wr=(w-a)/2;for(i=0;i<M;i++)cls[i]="#fff";function rcl(){return""+rh()+rh()+rh()+rh()+rh()+rh()}function rclh(){return"#"+rcl()}function rcls(){for(i=0;i<M;i++)cls[i]=rclh()}clb.onclick=()=>{clbi=!clbi;if(clbi){clb.classList.add("btna");cli.src="klw.svg";rcls();Rysuj_Trojkat_Sierpinskiego(k.value,p.value)}else{clb.classList.remove("btna");cli.src="kl.svg";for(i=0;i<M;i++)cls[i]="#fff";Rysuj_Trojkat_Sierpinskiego(k.value,p.value)}};k.value="f15f04";p.value="2";k.onfocus=()=>k.classList.remove("e");p.onfocus=()=>p.classList.remove("e");document.getElementById("l").onclick=()=>{k.classList.remove("e");p.classList.remove("e");k.value=rcl();if(clbi)rcls();else for(i=0;i<M;i++)cls[i]="#fff";var o=p.value,n;while((n=Math.floor(Math.random()*10))==o);p.value=n};document.getElementById("u").onclick=()=>{var kv=k.value,pv=p.value,b=1;if(kv.length!=6){k.classList.add("e");return};for(i=0;i<6;i++)if(!ih(kv[i])){b=0;break}if(!b){k.classList.add("e")}if(pv!="0"&&pv!="1"&&pv!="2"&&pv!="3"&&pv!="4"&&pv!="5"&&pv!="6"&&pv!="7"&&pv!="8"&&pv!="9"){p.classList.add("e");b=0}if(b)Rysuj_Trojkat_Sierpinskiego(kv,pv)};function rh(){var n=Math.floor(Math.random()*16);if(n==10)return"a";if(n==11)return"b";if(n==12)return"c";if(n==13)return"d";if(n==14)return"e";if(n==15)return"f";return n}function ih(h){return h=="0"||h=="1"||h=="2"||h=="3"||h=="4"||h=="5"||h=="6"||h=="7"||h=="8"||h=="9"||h=="a"||h=="A"||h=="b"||h=="B"||h=="c"||h=="C"||h=="d"||h=="D"||h=="e"||h=="E"||h=="f"||h=="F"}Rysuj_Trojkat_Sierpinskiego("f15f04",2);
 function Rysuj_Trojkat_Sierpinskiego(kolor, stopien) {
 	c.fillStyle="#"+kolor;
-	c.strokeStyle="#"+kolor;
 	triangle(wr,h,w/2,0,w-wr,h);
-	c.fillStyle="#fff";
-	c.strokeStyle="#fff";
 	Trojkat_Sierpinskiego(wr,h,w/2,0,w-wr,h,stopien);
 	
 	function Trojkat_Sierpinskiego(ax,ay,bx,by,cx,cy,st){
@@ -16,6 +13,7 @@ function Rysuj_Trojkat_Sierpinskiego(kolor, stopien) {
 		acy=(ay+cy)/2;
 		abx=(ax+bx)/2;
 		aby=(ay+by)/2;
+		c.fillStyle=cls[st-1];
 		triangle(abx,aby,bcx,bcy,acx,acy);
 		Trojkat_Sierpinskiego(ax,ay,abx,aby,acx,acy,st-1);
 		Trojkat_Sierpinskiego(abx,aby,bx,by,bcx,bcy,st-1);
@@ -25,9 +23,8 @@ function Rysuj_Trojkat_Sierpinskiego(kolor, stopien) {
 }
 function triangle(ax,ay,bx,by,cx,cy) {
 	c.beginPath();
-	c.lineTo(ax,ay);
+	c.moveTo(ax,ay);
 	c.lineTo(bx,by);
 	c.lineTo(cx,cy);
 	c.fill();
-	c.stroke();
 }
