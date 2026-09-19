7aa6975f59242172dd3abd21bedf52cce6c18a84
piotrzdziarski <pzdziarski@protonmail.com>
2026-09-19 09:18:24 +0200

    Improved randomizing color

--- klg.js
+++ klg.js
@@ -52,9 +52,9 @@ function removePointAndU(n) {
 	pts.removeChild(pts.children[n + 1]);
 	diab.removeChild(diab.children[n + 2])
 }
-document.getElementById("arl").onclick=arrow(-1);document.getElementById("arr").onclick=arrow(1);p.onfocus=()=>p.classList.remove("e");document.getElementById("l").onclick=()=>{k.classList.remove("e");p.classList.remove("e");k.value=rcl();if(clbi)rcls();cn.setAttribute("width",w+"px");for(let i=0;i<timeouts.length;i++)clearTimeout(timeouts[i]);timeouts=[];var o=parseInt(p.value),n;while((n=Math.floor(Math.random()*10))==o);p.value=n;opv=n;for(i=o-1;i>=-1;i--)removePointAndU(i);for(i=0;i<=n;i++)appendPointAndU(i,Math.floor(Math.random()*cn.width),Math.floor(Math.random()*cn.height))}
+document.getElementById("arl").onclick=arrow(-1);document.getElementById("arr").onclick=arrow(1);p.onfocus=()=>p.classList.remove("e");document.getElementById("l").onclick=()=>{k.classList.remove("e");p.classList.remove("e");k.value=rcl();if(clbi)rcls();else for(i=0;i<=M;i++)cls[i]="#"+k.value;cn.setAttribute("width",w+"px");for(let i=0;i<timeouts.length;i++)clearTimeout(timeouts[i]);timeouts=[];var o=parseInt(p.value),n;while((n=Math.floor(Math.random()*10))==o);p.value=n;opv=n;for(i=o-1;i>=-1;i--)removePointAndU(i);for(i=0;i<=n;i++)appendPointAndU(i,Math.floor(Math.random()*cn.width),Math.floor(Math.random()*cn.height))}
 document.getElementById("u").onclick=()=>{
-	var kv=k.value,pv=parseInt(p.value),b=1;if(kv.length!=6){k.classList.add("e");return};for(i=0;i<6;i++)if(!ih(kv[i])){b=0;break}if(!b){k.classList.add("e")}if(!clbi)for(i=0;i<=M;i++)cls[i]="#"+k.value;if(pv!=0&&pv!=1&&pv!=2&&pv!=3&&pv!=4&&pv!=5&&pv!=6&&pv!=7&&pv!=8&&pv!=9){p.classList.add("e");b=0}if(!b)return;
+	var kv=k.value,pv=parseInt(p.value),b=1;if(kv.length!=6){k.classList.add("e");return};for(i=0;i<6;i++)if(!ih(kv[i])){b=0;break}if(!b){k.classList.add("e")}if(pv!=0&&pv!=1&&pv!=2&&pv!=3&&pv!=4&&pv!=5&&pv!=6&&pv!=7&&pv!=8&&pv!=9){p.classList.add("e");b=0}if(!b)return;
 	if (pv > opv)
 		for (i = opv + 1; i <= pv; i++) appendPointAndU(i);
 	else
